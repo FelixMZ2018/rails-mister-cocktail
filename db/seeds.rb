4a1f4986ec6d00a7c676f4566f59ac9a1ffe2205
Felix Menzel <FelixinLondon@googlemail.com>
2020-08-14 11:46:34 +0200

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,13 @@ end
 puts "Ingredient list created"
 
 puts "Creating Cocktail List"
-Cocktail.create(name: "Bloody Mary")
-Cocktail.create(name: "Negroni")
-Cocktail.create(name: "Martini")
+
+### example seeds ###
+bd = {name: "Bloody Mary", description: "For I am a sinner in the hands of an angry god. Bloody Mary, full of vodka, blessed are you among cocktails. Pray for me now, at the hour of my death, which I hope is soon.",rating: rand(0..5)}
+ng = {name: "Negroni", description: "Easy to make and refreshingly bitter, the Negroni is said to have been invented in Florence by a dauntless Italian count who demanded that the bartender replace the club soda in his Americano with gin. It was a substitution that launched a thousand riffs.",rating: rand(0..5)}
+co = {name: "Cosmopolitan", description: "The legendary Cosmopolitan is a simple cocktail with a big history. It reached its height of popularity in the 1990s, when the HBO show “Sex and the City” was at its peak. The pink-hued Martini was a favorite of the characters on the show.",rating: rand(0..5)}
+
+Cocktail.create(bd)
+Cocktail.create(ng)
+Cocktail.create(co)
 
