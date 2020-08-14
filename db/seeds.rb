5b0f334adc460f2b3263c4b397c4e9ec3fde50f7
Felix Menzel <FelixinLondon@googlemail.com>
2020-08-14 11:54:20 +0200

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,7 @@ puts "Creating Cocktail List"
 bd = {name: "Bloody Mary", description: "For I am a sinner in the hands of an angry god. Bloody Mary, full of vodka, blessed are you among cocktails. Pray for me now, at the hour of my death, which I hope is soon.",rating: rand(0..5)}
 ng = {name: "Negroni", description: "Easy to make and refreshingly bitter, the Negroni is said to have been invented in Florence by a dauntless Italian count who demanded that the bartender replace the club soda in his Americano with gin. It was a substitution that launched a thousand riffs.",rating: rand(0..5)}
 co = {name: "Cosmopolitan", description: "The legendary Cosmopolitan is a simple cocktail with a big history. It reached its height of popularity in the 1990s, when the HBO show “Sex and the City” was at its peak. The pink-hued Martini was a favorite of the characters on the show.",rating: rand(0..5)}
+sf = {name: "Suffering Bastard", description: "So many classic cocktails are born in the bars of great hotels. Originally conceived as a hangover cure by the bar staff at Cairo’s Shepherd’s Hotel (before it burned to the ground in 1952), this bourbon- and gin-based drink has been around for 70 years. To adjust the potency—or get a jump on tomorrow’s hangover—simply increase or decrease the amount of ginger ale.",rating: rand(0..5)}
 
 Cocktail.create(bd)
 Cocktail.create(ng)
